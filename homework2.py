71a53d1ee9e00377232e2f50747f0c78bc3a3915
KA1_SA <swiftkey.vetrov@yandex.ru>
2024-06-21 23:14:08 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+The_number_of_completed_HW = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_fo_one_task = Number_of_hours_spent / The_number_of_completed_HW
+print( Course_name,', всего задач:',The_number_of_completed_HW, ', затрачено часов: ',Number_of_hours_spent, ', среднее время выполнения', Time_fo_one_task,'часа.')
